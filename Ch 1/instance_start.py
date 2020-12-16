494eb4bf40fbda0ec4e3b0af3ebd59e48a3c2455
akioorihara <56176776+akioorihara@users.noreply.github.com>
2020-12-16 14:40:51 -0600

    GetDiscount function

--- Ch 1/instance_start.py
+++ Ch 1/instance_start.py
@@ -14,18 +14,26 @@ class Book:
 
     # TODO: create instance methods
     def getPrice(self):
-        return self.price 
+        if hasattr(self, "_discount"):
+            return self.price - (self.price * self._discount)
+        else:
+            return self.price 
 
+    def getDiscount(self, amount):
+        self._discount = amount 
+ 
 # TODO: create some book instances
 b1 = Book("War and Peace", "Akio Orihara", 99, 2.00)
-b2 = Book("The Catcher in the Rye", "Ashley", 11, 11.0)
+b2 = Book("The Catcher in the Rye", "Ashley", 11, 29.95)
 
 # TODO: print the price of book1
-print("The price is :", b1.price)
-print(b1.getPrice())
-print(b2.getPrice())
+#print("The price is :", b1.price)
+#print(b1.getPrice())
+#print(b2.getPrice())
 
 # TODO: try setting the discount
-
+print(b2.getPrice())
+b2.getDiscount(0.25)
+print(b2.getPrice())
 
 # TODO: properties with double underscores are hidden by the interpreter
