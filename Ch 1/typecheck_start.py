10a58f9a6e3e101b9705ab25ce97f8ddfe1a2b1c
akioorihara <56176776+akioorihara@users.noreply.github.com>
2020-12-16 21:25:52 -0600

    trying with "isinstance"
    it takes 2 args, object, and class

--- Ch 1/typecheck_start.py
+++ Ch 1/typecheck_start.py
@@ -19,8 +19,8 @@ n1 = Newspaper("The Washington Post")
 n2 = Newspaper("The New York Times")
 
 # TODO: use type() to inspect the object type
-print(type(b1))
-print(type(n1))
+# print(type(b1))
+# print(type(n1))
 
 # TODO: compare two types together
 # def compareDataType(self): 
@@ -28,7 +28,11 @@ print(type(n1))
 #         print("The same data type")
 #     else:
 #         print("not the same data type")
-print(type(b1) == type(b2))
-print(type(n1) == type(b1))
+# print(type(b1) == type(b2))
+# print(type(n1) == type(b1))
 
 # TODO: use isinstance to compare a specific instance to a known type
+print(isinstance(b1, Book))
+print(isinstance(n1, Newspaper))
+print(isinstance(n2, Book))
+print(isinstance(n1, object))
