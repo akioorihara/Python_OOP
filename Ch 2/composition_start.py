63f7161e07ccf2cadbf26907a4056bf34d5061fc
akioorihara <56176776+akioorihara@users.noreply.github.com>
2020-12-22 21:22:22 -0600

    complex composition

--- Ch 2/composition_start.py
+++ Ch 2/composition_start.py
@@ -1,17 +1,14 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using composition to build complex objects
-
 class Book:
     def __init__(self, title, price, author=None):
         self.title = title
         self.price = price
-
         self.author = author 
-
         self.chapters = []
 
-    def addchapter(self, chapter):
-        self.chapters.append((chapter ))
+    def addchapter(self,chapter):
+        self.chapters.append((chapter))
 
     def getBookPageCount(self):
         result = 0
@@ -42,4 +39,4 @@ b1.addchapter(Chapter("Chapter 3", 143))
 print(b1.title)
 print(b1.getBookPageCount())
 print(b1.author)
-print(b1.chapters)
+print(b1.chapters[0])
