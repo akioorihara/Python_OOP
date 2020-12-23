704ec9715c50322d1a2668e963a199301f2f133b
akioorihara <56176776+akioorihara@users.noreply.github.com>
2020-12-23 08:30:18 -0600

    __eq__ is implimented

--- Ch 3/magiceq_start.py
+++ Ch 3/magiceq_start.py
@@ -10,7 +10,12 @@ class Book:
         self.price = price
 
     # TODO: the __eq__ method checks for equality between two objects
-
+    def __eq__(self, value):
+       # print("__eq__ is called")
+        if not isinstance(value, Book):
+            raise ValueError("Can't compare a value with Book value")
+        else:
+            return (self.title == value.title and self.author == value.author and self.price == value.price)
     # TODO: the __ge__ establishes >= relationship with another obj
 
     # TODO: the __lt__ establishes < relationship with another obj
@@ -22,7 +27,7 @@ b3 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b4 = Book("To Kill a Mockingbird", "Harper Lee", 24.95)
 
 # TODO: Check for equality
-
+print(b1 == b3)
 
 # TODO: Check for greater and lesser value
 
