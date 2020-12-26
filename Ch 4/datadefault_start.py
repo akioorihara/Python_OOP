3283e29ddfe84479527188504c9f3c5fdc8c0770
akioorihara <56176776+akioorihara@users.noreply.github.com>
2020-12-26 11:41:45 -0600

    Dataclasses in Python
    Leaning dataclasses()

--- Ch 4/datadefault_start.py
+++ Ch 4/datadefault_start.py
@@ -1,13 +1,22 @@
 # Python Object Oriented Programming by Joe Marini course example
 # implementing default values in data classes
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+import random   
 
+def price_func():
+    return float(random.randrange(20, 40))
 
 @dataclass
 class Book:
     # you can define default values when attributes are declared
-    title: str
-    author: str
-    pages: int
-    price: float
+    title: str = "No Title"
+    author: str = "No Author"
+    pages: int = 0 
+    price: float = field(default_factory=price_func)
+
+
+b1 = Book("Annymous", "Akio", 123)
+b2 = Book("We will be the one!", "Taylor", 456)
+
+print(b1, b2)
